b035705b3637106102d93af4770500d830b97bce
Nico Arato <nico.arato@gmail.com>
2021-01-04 21:31:09 -0300

    Usando currencyPipe.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'filtros';
+
+  nombreEmpresa = 'marketing digital';
+  titulo = 'paquetes';
+  fecha = new Date();
+
+  // paquetes = {titulo : 'Básico', precio : 100 , mes: 'mes', d1: ' Redes Sociales', d2: 'Branding', d4: 'Diseño de imagen'};
+
+  paquetes = [
+    {
+      titulo : 'Básico', 
+      precio : 100 , 
+      mes: 'mes', 
+      d1: ' Redes Sociales', 
+      d2: 'Branding', 
+      d3: 'Diseño de imagen', 
+      d4: '-'
+    },
+    {
+      titulo : 'Recomendado', 
+      precio : 200 , 
+      mes: 'mes', 
+      d1: ' Redes Sociales', 
+      d2: 'Branding', 
+      d3: 'Video', 
+      d4: 'Diseño de imagen'
+    },
+    {
+      titulo : 'Avanzado', 
+      precio : 300 , 
+      mes: 'mes', 
+      d1: ' Redes Sociales', 
+      d2: 'Branding', 
+      d3: 'Imagen/Video', 
+      d4: 'Comunity Manager'
+    },
+  ]
 }
